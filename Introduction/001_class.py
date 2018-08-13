d34466d754575d0adaac02b7d1e0820203d9e929
Varsha Ram kumar <varsharamkumar@Varshas-MacBook-Pro.local>
2018-08-13 06:53:30 -0400

    Initial creation

---
+++ Introduction/001_class.py
@@ -0,0 +1,22 @@
+# defining a class
+class Employee:
+    def __init__(self, first, last, pay): # self defines passing instance of a class
+        self.fname = first
+        self.lname = last
+        self.pay = pay
+        self.email = self.fname + "."+self.lname+"@company.com"
+
+    def get_fname(self):# self defines passing instance of a class
+        return '{} {}'.format(self.fname,self.lname)
+
+# class instance 
+# variables and functions inside a class are instance variables 
+# and instance methods
+
+emp1 = Employee('Ram','Kumar','70000')
+emp2 = Employee('Test','User','80000')
+
+print(emp1.get_fname()) # here instance is automatically passed
+print(Employee.get_fname(emp2)) # equivalent of above statement. 
+# Passing instance manually. this is behind the scene how instance method works.
+#  
