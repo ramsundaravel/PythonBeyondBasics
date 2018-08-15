46597fc9b7c136405de79a1ddc4549be7f8ce1ff
Varsha Ram kumar <varsharamkumar@Varshas-MacBook-Pro.local>
2018-08-15 08:11:45 -0400

    initial creation

---
+++ Misc/001_args_kwargs.py
@@ -0,0 +1,19 @@
+# args - refers to wrapping position arguments to tuple
+# **kwargs - refers to wrapping key = value arguments to dict
+
+def foo(required, *args, **kwargs):
+    if required:
+        print(required)
+    if args:
+        print(args)
+        # print('unpacks :',*args) # unpacks to positional arguments
+    if kwargs:
+        print(kwargs)
+
+foo('Ram')
+foo('Ram',1,2,3,4,5)
+foo('Ram',1,2,3,4,5,key1='value1')
+
+# important to understant
+# * and ** in function definition refers to packing positional or kv pairs to tuple or list
+# * and ** infront of list or dictionary unpacks item to positional arguments 
