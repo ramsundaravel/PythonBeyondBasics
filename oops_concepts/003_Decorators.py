d712f258883c08ac0291ddece8ede236d642e904
Varsha Ram kumar <varsharamkumar@Varshas-MacBook-Pro.local>
2018-08-16 07:55:19 -0400

    decorators

--- oops_concepts/003_Decorators.py
+++ oops_concepts/003_Decorators.py
@@ -1,4 +1,5 @@
 # Decorators - comes when a function is passed 
+# maintain the additional functionalities in one place and resuse any number of times whereever needed
 # For example,below we are passing display function --> orginal function parameter
 # and display_decorator --> wrapper(). (as per first class function concept)
 # per closure concept, wrapper holds information present in orginal function
@@ -44,17 +45,56 @@ print('\ndecorators with arguments passed')
 
 #Example 2
 def decorator_function(original_function):
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs): # advantage - optional params and not show error even if not passed
         print('Executing function - {}'.format(original_function.__name__))
         original_function(*args,**kwargs)
+        print('\n')
     return wrapper
+
+@decorator_function
+def display():
+    print('display function ran')
 
 @decorator_function
 def display_info(name, age):
     print('My name is {name} and age is {age}'.format(name=name, age=age))
 
-
+display()
 display_info(name = 'ram',age = 30)
+
+
+#--------------------------------------------------------------------------------------------------
+print('Decorator class')
+# decorator class
+# Rarely used
+
+class decorator_class(object):
+    def __init__(self, original_function):
+        self.original_function = original_function
+
+    def __call__(self,*args, **kwargs): ## call method makes an instance executable
+        # closure concepts applies over here
+        print('call method executed before this {}'.format(self.original_function.__name__))
+        self.original_function(*args, **kwargs)
+
+#---------
+# # information about __call__method__
+# # call method makes the instance executable
+# def display_info1(name, age):
+#     print('My name is {name} and age is {age}'.format(name=name, age=age))
+
+# a = decorator_class(display_info1)
+# a('ram','test')
+#---------
+
+@decorator_class
+def display_info(name, age):
+     print('My name is {name} and age is {age}'.format(name=name, age=age))
+
+display_info('Ram',31)
+#--------------------------------------------------------------------------------------------------
+
+
 
 
 
